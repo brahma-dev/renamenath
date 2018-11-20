ebac7822779b93c391b7bcd98b7449ee8be0b7b6
Brahma Dev <me@brahma.world>
2018-11-20 20:25:33 +0530

    Replace whole words only

--- src/content.js
+++ src/content.js
@@ -9,22 +9,22 @@
 
 			if (node.nodeType === 3) {
 				var text = node.nodeValue;
-				var replacedText = text.replace(/Yogi Adityanath/g, "Ajay Mohan Bisht")
-					.replace(/YogiAdityanath/g, "AjayMohanBisht")
-					.replace(/yogi adityanath/g, "ajay mohan bisht")
-					.replace(/yogiadityanath/g, "ajaymohanbisht")
-					.replace(/CM Yogi/g, "CM Ajay")
-					.replace(/Prayagraj/g, "Allahabad")
-					.replace(/prayagraj/g, "allahabad")
-					.replace(/Ayodhya/g, "Faizabad")
-					.replace(/ayodhya/g, "faizabad")
-					.replace(/\u092F\u094B\u0917\u0940 \u0906\u0926\u093F\u0924\u094D\u092F\u0928\u093E\u0925/g, "\u0905\u091C\u092F \u092E\u094B\u0939\u0928 \u092C\u093F\u0937\u094D\u091F")
-					.replace(/\u092F\u094B\u0917\u0940\u0906\u0926\u093F\u0924\u094D\u092F\u0928\u093E\u0925/g, "\u0905\u091C\u092F\u092E\u094B\u0939\u0928\u092C\u093F\u0937\u094D\u091F")
-					.replace(/\u092F\u094B\u0917\u0940 \u0906\u0926\u093F\u0924\u094D\u200D\u092F\u0928\u093E\u0925/g, "\u0905\u091C\u092F \u092E\u094B\u0939\u0928 \u092C\u093F\u0937\u094D\u091F")
-					.replace(/\u092F\u094B\u0917\u0940\u0906\u0926\u093F\u0924\u094D\u200D\u092F\u0928\u093E\u0925/g, "\u0905\u091C\u092F\u092E\u094B\u0939\u0928\u092C\u093F\u0937\u094D\u091F")
-					.replace(/\u0938\u0940\u090F\u092E \u092F\u094B\u0917\u0940/g, "\u0938\u0940\u090F\u092E \u0905\u091C\u092F")
-					.replace(/\u092A\u094D\u0930\u092F\u093E\u0917\u0930\u093E\u091C/g, "\u0905\u0932\u093E\u0939\u093E\u092C\u093E\u0926")
-					.replace(/\u0905\u092F\u094B\u0927\u094D\u092F\u093E/g, "\u092B\u093C\u0948\u091C\u093C\u093E\u092C\u093E\u0926");
+				var replacedText = text.replace(/\bYogi Adityanath\b/g, "Ajay Mohan Bisht")
+					.replace(/\bYogiAdityanath\b/g, "AjayMohanBisht")
+					.replace(/\byogi adityanath\b/g, "ajay mohan bisht")
+					.replace(/\byogiadityanath\b/g, "ajaymohanbisht")
+					.replace(/\bCM Yogi\b/g, "CM Ajay")
+					.replace(/\bPrayagraj\b/g, "Allahabad")
+					.replace(/\bprayagraj\b/g, "allahabad")
+					.replace(/\bAyodhya\b/g, "Faizabad")
+					.replace(/\bayodhya\b/g, "faizabad")
+					.replace(/\b\u092F\u094B\u0917\u0940 \u0906\u0926\u093F\u0924\u094D\u092F\u0928\u093E\u0925\b/g, "\u0905\u091C\u092F \u092E\u094B\u0939\u0928 \u092C\u093F\u0937\u094D\u091F")
+					.replace(/\b\u092F\u094B\u0917\u0940\u0906\u0926\u093F\u0924\u094D\u092F\u0928\u093E\u0925\b/g, "\u0905\u091C\u092F\u092E\u094B\u0939\u0928\u092C\u093F\u0937\u094D\u091F")
+					.replace(/\b\u092F\u094B\u0917\u0940 \u0906\u0926\u093F\u0924\u094D\u200D\u092F\u0928\u093E\u0925\b/g, "\u0905\u091C\u092F \u092E\u094B\u0939\u0928 \u092C\u093F\u0937\u094D\u091F")
+					.replace(/\b\u092F\u094B\u0917\u0940\u0906\u0926\u093F\u0924\u094D\u200D\u092F\u0928\u093E\u0925\b/g, "\u0905\u091C\u092F\u092E\u094B\u0939\u0928\u092C\u093F\u0937\u094D\u091F")
+					.replace(/\b\u0938\u0940\u090F\u092E \u092F\u094B\u0917\u0940\b/g, "\u0938\u0940\u090F\u092E \u0905\u091C\u092F")
+					.replace(/\b\u092A\u094D\u0930\u092F\u093E\u0917\u0930\u093E\u091C\b/g, "\u0905\u0932\u093E\u0939\u093E\u092C\u093E\u0926")
+					.replace(/\b\u0905\u092F\u094B\u0927\u094D\u092F\u093E\b/g, "\u092B\u093C\u0948\u091C\u093C\u093E\u092C\u093E\u0926");
 
 				if (replacedText !== text) {
 					element.replaceChild(document.createTextNode(replacedText), node);
